bfad8b205630d32364865e70c758efef95a982ff
Uzair Shahid <uzairu809@gmail.com>
2023-10-06 22:38:22 +0500

    Q2 FirstNonRepeating Done!

--- src/FirstNonRepeating/FirstNonRepeating.java
+++ src/FirstNonRepeating/FirstNonRepeating.java
@@ -1,4 +1,41 @@
 package FirstNonRepeating;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FirstNonRepeating {
+    private MyHashTable myHashTable;
+    private ArrayList<Integer> arrayList;
+    public FirstNonRepeating(){
+        myHashTable=new MyHashTable();
+        arrayList=new ArrayList<>();
+    }
+
+
+    public void input(){
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.next();
+        for (int i=0;i<s.length();i++) {
+            int x = -1;
+            if ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')) {
+                x = myHashTable.put(s.charAt(i));
+            }
+            if (x == 0) {
+                if (arrayList.contains(myHashTable.getID(s.charAt(i)))) {
+                    arrayList.remove(arrayList.indexOf(myHashTable.getID(s.charAt(i))));
+                }
+            }
+            else  if(x==1){
+                arrayList.add(myHashTable.getID(s.charAt(i)));
+            }
+        }
+    }
+
+    public char getFirstNonRepeating() {
+        if (!arrayList.isEmpty()) {
+            return myHashTable.getChar(arrayList.get(0));
+        }
+        return '-';
+    }
 
 }
---
+++ src/FirstNonRepeating/FirstNonRepeatingExecution.java
@@ -0,0 +1,9 @@
+package FirstNonRepeating;
+
+public class FirstNonRepeatingExecution {
+    public static void main(String[] args) {
+        FirstNonRepeating firstNonRepeating=new FirstNonRepeating();
+        firstNonRepeating.input();
+        System.out.println(firstNonRepeating.getFirstNonRepeating());
+    }
+}
